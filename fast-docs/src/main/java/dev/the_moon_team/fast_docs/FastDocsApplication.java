18803d5c5fd3944c508e8fd7b67e217d743ba180
48-iq <ivn25ov@mail.ru>
2024-12-15 21:08:25 +0400

    initial commit

---
+++ fast-docs/src/main/java/dev/the_moon_team/fast_docs/FastDocsApplication.java
@@ -0,0 +1,13 @@
+package dev.the_moon_team.fast_docs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastDocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastDocsApplication.class, args);
+	}
+
+}
---
+++ fast-docs/src/test/java/dev/the_moon_team/fast_docs/FastDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.the_moon_team.fast_docs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastDocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
