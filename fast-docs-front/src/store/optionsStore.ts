ccf36c6f481429ded059283182a475897eb29bc6
Ilya Ivanov <ivn25ov@mail.ru>
2024-12-22 20:40:24 +0400

    save progress

---
+++ fast-docs-front/src/store/optionsStore.ts
@@ -0,0 +1,11 @@
+
+import { defineStore } from "pinia"
+import { ref } from "vue"
+
+
+export const useOptionsStore = defineStore('optionsStore', () => {
+  const selectedTemplate = ref('')
+  
+
+  return { }
+})
